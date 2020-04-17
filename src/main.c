4f07b2cee6d6c8f73e5a921fdfb326b40dd231a0
ozkanyldrm55 <58321853+ozkanyldrm55@users.noreply.github.com>
2020-04-17 16:32:43 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-
+/* Potansiyometre ile ADC okumasi yapiliyor ve okunan degerler DAC a aktariliyor */
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 
@@ -7,77 +7,65 @@ ADC_InitTypeDef ADC_InitStruct;
 ADC_CommonInitTypeDef ADC_CommonInitStruct;
 DAC_InitTypeDef DAC_InitStruct;
 
-void GPIO_Config()
-{
+void GPIO_Config() {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	
+	// ADC okumasi yapilacak olan pin
+	GPIO_Struct.GPIO_Mode 	= GPIO_Mode_AN;
+	GPIO_Struct.GPIO_OType 	= GPIO_OType_PP;
+	GPIO_Struct.GPIO_Pin 	= GPIO_Pin_0;
+	GPIO_Struct.GPIO_PuPd 	= GPIO_PuPd_NOPULL;
+	GPIO_Struct.GPIO_Speed 	= GPIO_Speed_100MHz;
 
-	GPIO_Struct.GPIO_Mode = GPIO_Mode_AN;
-	GPIO_Struct.GPIO_OType = GPIO_OType_PP;
-	GPIO_Struct.GPIO_Pin = GPIO_Pin_0;
-	GPIO_Struct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Struct.GPIO_Speed = GPIO_Speed_100MHz;
-
-	GPIO_Init(GPIOA,&GPIO_Struct);
-
-	GPIO_Struct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_Struct.GPIO_OType = GPIO_OType_PP;
-	GPIO_Struct.GPIO_Pin = GPIO_Pin_4;
-	GPIO_Struct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Struct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOA , &GPIO_Struct);
+	
+	// DAC cikis pini
+	GPIO_Struct.GPIO_Mode 	= GPIO_Mode_OUT;
+	GPIO_Struct.GPIO_OType 	= GPIO_OType_PP;
+	GPIO_Struct.GPIO_Pin 	= GPIO_Pin_4;
+	GPIO_Struct.GPIO_PuPd 	= GPIO_PuPd_NOPULL;
+	GPIO_Struct.GPIO_Speed 	= GPIO_Speed_100MHz;
 
 	GPIO_Init(GPIOA,&GPIO_Struct);
 }
-void ADC_Config()
-{
-	RCC_APB2PeriphClockCmd(RCC_APB1Periph_DAC,ENABLE);
+void ADC_Config() {
+	RCC_APB2PeriphClockCmd(RCC_APB1Periph_DAC,ENABLE); 	 // Clock hattini aktif ettik.
 
-	ADC_CommonInitStruct.ADC_Mode = ADC_Mode_Independent;
-	ADC_CommonInitStruct.ADC_Prescaler = ADC_Prescaler_Div4;
+	ADC_CommonInitStruct.ADC_Mode = ADC_Mode_Independent; 	 // bagimsiz mod da calisma aktif edildi.
+	ADC_CommonInitStruct.ADC_Prescaler = ADC_Prescaler_Div4; // adc max 36 MHz degerinde calisir.Suanda 168 MHz de calistigimiz icin 4 e bolduk.
+	ADC_CommonInit(ADC1,&ADC_CommonInitStruct); 		 // adc degeri isleniyor.
 
-	ADC_Init(ADC1,&ADC_CommonInitStruct);
-
-
-	ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;
-
+	ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;	 // Coklu ADC okumasi yapmadigimiz icin sadece cozunurlugu ayarlamamiz yeterli olacak.
 	ADC_Init(ADC1,&ADC_InitStruct);
-	ADC_Cmd(ADC1,ENABLE);
+	ADC_Cmd(ADC1,ENABLE); 					 // ADC cevresel birimi oldugu icin cmd kullanarak aktif etmemiz gerekir.
 }
-void DAC1_Config()
-{
+void DAC1_Config() {
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC,ENABLE);
 
-	DAC_InitStruct.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
-	DAC_InitStruct.DAC_Trigger = DAC_Trigger_None;
-	DAC_InitStruct.DAC_WaveGeneration = DAC_WaveGeneration_None;
-
+	DAC_InitStruct.DAC_OutputBuffer = DAC_OutputBuffer_Enable; 	// gurultu filtreleyici aktif edildi.
+	DAC_InitStruct.DAC_Trigger = DAC_Trigger_None; 			// tetikleme olmasin
+	DAC_InitStruct.DAC_WaveGeneration = DAC_WaveGeneration_None; 	// dalga uretici olmasin
 	DAC_Init(DAC_Channel_1,&DAC_InitStruct);
-	DAC_Cmd(DAC_Channel_1,ENABLE);
+	DAC_Cmd(DAC_Channel_1,ENABLE); 					// DAC cevresel birim
 }
 
 uint16_t adc_value;
 
-uint16_t Read_ADC()
-{
+uint16_t Read_ADC() { // 12 bit degerinde adc okudugumuz icin 16 bit geri donus degeri olan adc fonk yazmaliyiz.
 	ADC_RegularChannelConfig(ADC1,ADC_Channel_1,1,ADC_SampleTime_56Cycles);
 	ADC_SoftwareStartConv(ADC1);
 
-	while(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC) == RESET);
-
-	return ADC_GetConversionValue(ADC1);
+	while(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC) == RESET); // reset ise buradan cik degil ise reset olana kadar bekle
+	return ADC_GetConversionValue(ADC1); 		      // ADC de okunan degeri geri donderiyoruz.
 }
-
-
-int main(void)
-{
+int main(void) {
 	GPIO_Config();
 	ADC_Config();
 	DAC1_Config();
 
-  while (1)
-  {
+  while (1) {
 	  adc_value = Read_ADC();
-
-	  DAC_SetChannel1Data(DAC_Align_12b_R,adc_value);//adc de okunan degeri dac a atiyoruz
+	  DAC_SetChannel1Data(DAC_Align_12b_R,adc_value); // adc de okunan degeri dac a atiyoruz
   }
 }
 
